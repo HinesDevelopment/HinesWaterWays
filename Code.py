c99c07fafa3c68bdf7a68a8dadd60fc91c1ef218
HinesDevelopment <149387491+HinesDevelopment@users.noreply.github.com>
2023-10-31 09:19:33 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -25,3 +25,33 @@ login_button = tk.Button(root, text="Login", command=check_login)
 login_button.pack()
 
 root.mainloop()
+import random
+import tkinter as tk
+
+# Define the slot machine symbols
+symbols = ["cherry", "lemon", "orange", "plum", "bell", "bar", "seven"]
+
+def spin():
+    results = [random.choice(symbols) for _ in range(3)]
+    # Display the results and calculate the payout
+    # You can use a Canvas widget to display the symbols and handle animations
+    # Implement the payout logic based on your requirements
+
+# Define the slot machine game GUI
+def slot_machine_game():
+    game_window = tk.Toplevel(root)
+    game_window.title("Slot Machine Game")
+
+    spin_button = tk.Button(game_window, text="Spin", command=spin)
+    spin_button.pack()
+
+    # Add Canvas widget to display the slot machine symbols
+
+    # Implement payout logic
+
+root = tk.Tk()
+root.title("Sign In")
+
+# ... Sign-in GUI as mentioned earlier ...
+
+root.mainloop()
